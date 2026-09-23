dd4d569fefd92619459bfa37bf0e30f8e09f45e1
bowlperson <61048483+bowlperson@users.noreply.github.com>
2026-09-23 16:56:09 -0700

    Anchor reminders to Tucson MST

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'viki-v1.10.3';
+const CACHE_NAME = 'viki-v1.10.4';
 const APP_SHELL = ['./', './Index.html', './viki-personality.js', './manifest.json'];
 
 self.addEventListener('install', event => {
