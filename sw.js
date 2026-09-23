5fea79c625330ad4f899d5d7e696d05f6fe51be7
bowlperson <61048483+bowlperson@users.noreply.github.com>
2026-09-23 16:10:05 -0700

    Keep reminder registry title on one line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'viki-v1.10.0';
+const CACHE_NAME = 'viki-v1.10.1';
 const APP_SHELL = ['./', './Index.html', './viki-personality.js', './manifest.json'];
 
 self.addEventListener('install', event => {
