9bb427bbfb7ea7143392e915823dcca6d9ccb556
bowlperson <61048483+bowlperson@users.noreply.github.com>
2026-09-24 16:03:17 -0700

    Fix tomorrow weather question routing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'viki-v1.12.4';
+const CACHE_NAME = 'viki-v1.12.5';
 const APP_SHELL = ['./', './index.html', './viki-personality.js', './manifest.json'];
 
 self.addEventListener('install', event => {
