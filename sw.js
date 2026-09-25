25669052cac852e9965b0f240ec9d97615876e1d
bowlperson <61048483+bowlperson@users.noreply.github.com>
2026-09-25 16:57:20 -0700

    Add storage colors and Supabase cash profiles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'viki-v1.12.6';
+const CACHE_NAME = 'viki-v1.13.0';
 const APP_SHELL = ['./', './index.html', './viki-personality.js', './manifest.json'];
 
 self.addEventListener('install', event => {
